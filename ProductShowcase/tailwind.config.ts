b936d93f749bc0b50882f069713cf886da083823
TiagoLeopoldo <tnleopoldo.dev@gmail.com>
2026-03-05 14:36:49 -0300

    chore: configura TailwindCSS

--- ProductShowcase/tailwind.config.ts
+++ ProductShowcase/tailwind.config.ts
@@ -1,9 +1,20 @@
-import type { Config } from 'tailwindcss'
-
+/** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}',
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  plugins: []
-} satisfies Config
+  theme: {
+    extend: {
+      colors: {
+        pokemon: {
+          red: '#ff0000',
+          blue: '#3b4cca',
+          yellow: '#ffde00',
+          gold: '#b3a125'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
